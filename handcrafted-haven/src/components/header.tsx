fd1547d3e244d554e8eb0b273639895f5528e7cb
Paper-Dad <broberts@byupathway.edu>
2026-07-29 19:52:40 -0600

    added review page

---
+++ handcrafted-haven/src/components/header.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <header className="site-header">
+            <div className="header-content">
+                <Link href="/" className="site-logo">
+                    Handcrafted Haven
+                </Link>
+
+                <nav aria-label="Main navigation">
+                    <ul className="nav-links">
+                        <li>
+                            <Link href="/">Home</Link>
+                        </li>
+
+                        <li>
+                            <Link href="/products">Products</Link>
+                        </li>
+
+                        <li>
+                            <Link href="/artisans">Artisans</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+}
